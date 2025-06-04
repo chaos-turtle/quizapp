6f685c256b6325ef934d642d2cc6a2936ea20f2d
Stefan <stefanmisoski@gmail.com>
2025-06-04 13:40:28 +0200

    Initial urls and templates

--- QuizApp/PNUV/urls.py
+++ QuizApp/PNUV/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
@@ -22,4 +22,7 @@ import QuizApp.views as appViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', appViews.index, name='index'),
+    path('', appViews.index, name='default'),
+    path('quiz/', appViews.quiz, name='quiz'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
