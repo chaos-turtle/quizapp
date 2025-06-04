05e084c6aeaad601d11791473a9cc070cbd35e74
Stefan <stefanmisoski@gmail.com>
2025-06-04 13:58:04 +0200

    Initial views and forms

--- QuizApp/QuizApp/views.py
+++ QuizApp/QuizApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .forms import *
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html', context={'app_name': 'Quiz App'})
+
+def quiz(request):
+    quiz_obj = Quiz.objects.first()
+    return render(request, 'quiz.html', context={'app_name': 'Quiz App', 'quiz': quiz_obj})
